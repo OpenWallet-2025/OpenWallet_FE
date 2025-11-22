07f89feebbbe985211ded937d08476b32b36ab72
최현웅 <maetan.s200406@ggh.goe.go.kr>
2025-11-22 15:45:42 +0900

    design and profile are added

---
+++ HomeScreen/Profile.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".toggle-group").forEach(group => {
+    const buttons = group.querySelectorAll(".toggle-btn");
+
+    buttons.forEach(btn => {
+      btn.addEventListener("click", () => {
+        buttons.forEach(b => b.classList.remove("toggle-btn-active"));
+        btn.classList.add("toggle-btn-active");
+      });
+    });
+  });
+});
